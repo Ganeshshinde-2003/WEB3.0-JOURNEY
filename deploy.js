03f4427ee4cfe4022cabd1b212fb2988a86a0bca
Ganesh Shinde <sindheganesh2004@gmail.com>
2023-11-03 20:52:07 +0530

    Created first contract and deployed on actuall blockchian of sepolia

--- deploy.js
+++ deploy.js
@@ -1,38 +1,65 @@
-const { ethers } = require("ethers");
-const fs = require("fs-extra");
+const { ethers } = require("ethers")
+const fs = require("fs-extra")
+require("dotenv").config()
 
 async function main() {
   //compile them in our code
   //compile them separately
-  const provider = new ethers.providers.JsonRpcProvider(
-    "http://127.0.0.1:7545"
-  );
+  const provider = new ethers.providers.JsonRpcProvider(process.env.LOCALHOST)
+  // const encryptedJson = fs.readFileSync("./.encryptdKey.json", "utf-8");
 
-  const wallet = new ethers.Wallet(
-    "0x0bcde1ad8d4100044cb2842cd71354debaf17912723919bccc1eaf05e7887d4a",
-    provider
-  );
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
 
-  const abi = fs.readFileSync(
-    "./_SimpleStorage_sol_SimpleStorage.abi",
-    "utf-8"
-  );
+  // let wallet = new ethers.Wallet.fromEncryptedJsonSync(
+  //   encryptedJson,
+  //   process.env.PASSWORD
+  // );
+
+  // wallet = await wallet.connect(provider);
+  const abi = fs.readFileSync("./_SimpleStorage_sol_SimpleStorage.abi", "utf-8")
   const binary = fs.readFileSync(
     "./_SimpleStorage_sol_SimpleStorage.bin",
-    "utf-8"
-  );
+    "utf-8",
+  )
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet)
+  console.log("Deploying, Please wait....")
+  const contract = await contractFactory.deploy()
+  await contract.deployTransaction.wait(1)
+  console.log(`Contract Address: ${contract.address}`)
+  // console.log("Here is the deployemt transaction: ");
+  // console.log(contract.deployTransaction);
+  // console.log("Here is the transaction receipt: ");
+  // console.log(transactionReceipt);
 
-  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  // console.log("Let's deploy with only transaction data!");
+  // const nonce = await wallet.getTransactionCount();
+  // const tx = {
+  //   nonce: nonce,
+  //   gasPrice: 20000000000,
+  //   gasLimit: 6721975,
+  //   to: null,
+  //   value: 0,
+  //   data: `0x${binary}`,
+  //   chainId: 1337,
+  // };
 
-  console.log("Deploying, Please wait....");
+  // const sendTxResponce = await wallet.sendTransaction(tx);
+  // await sendTxResponce.wait(1);
+  // console.log(sendTxResponce);
 
-  const contract = await contractFactory.deploy();
-  console.log(contract);
+  const currentFavoriteNumber = await contract.retrview()
+  console.log(`Current Favorite Number: ${currentFavoriteNumber.toString()}`)
+
+  const transactionResponse = await contract.store(7)
+  const transactionReceipt = await transactionResponse.wait(1)
+
+  const updatedFavoriteNumber = await contract.retrview()
+  console.log(`Updated Favorite Number: ${updatedFavoriteNumber.toString()}`)
 }
 
 main()
   .then(() => process.exit(0))
   .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    console.error(error)
+    process.exit(1)
+  })
---
+++ encryptKey.js
@@ -0,0 +1,21 @@
+const ethers = require("ethers")
+const fs = require("fs-extra")
+require("dotenv").config()
+
+async function main() {
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY)
+  const encryptedJsonKey = await wallet.encrypt(
+    process.env.PRIVATE_KEY,
+    process.env.PASSWORD,
+  )
+
+  console.log(encryptedJsonKey)
+  fs.writeFileSync("./.encryptdKey.json", encryptedJsonKey)
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.log(error)
+    process.exit(1)
+  })
